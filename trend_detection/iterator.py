3a16acac44e61cb377bec03c2a800aecf471f0a8
jkbngl <jakob.engl@hotmail.de>
2021-03-23 22:39:15 +0100

    more clever way of iterating

--- trend_detection/iterator.py
+++ trend_detection/iterator.py
@@ -1,7 +1,13 @@
 import itertools
 
-stuff = ['itemgroup__category', 'workcenter__category',
-         'orgarea__category', 'site__category', 'nqc__val', 'scrap__val', 'nqcc__date']
-for L in range(0, len(stuff)+1):
-    for subset in itertools.combinations(stuff, L):
-        print(subset)
+
+def combinations(cols):
+    stuff = []
+
+    for L in range(0, len(cols)+1):
+        for subset in itertools.combinations(cols, L):
+
+            if len(list(subset)) > 0:
+                stuff.append(list(subset))
+
+    return stuff
--- trend_detection/query_builder.py
+++ trend_detection/query_builder.py
@@ -1,22 +1,25 @@
+import iterator
+
 
 columns = ['itemgroup__category', 'workcenter__category',
            'orgarea__category', 'site__category', 'nqc__val', 'scrap__val', 'nqcc__date']
 
+categoryCombinations = iterator.combinations(
+    [col for col in columns if col.endswith('__category')])
+
+print(categoryCombinations)
+
 timeframes = [5, 14, 21, 30, 60, 90, 180]
 
-for col in columns:
-    if col.endswith('__category'):
-        for pos in columns:
+for col in categoryCombinations:
 
-            vals = [valCol for valCol in columns if valCol.endswith('__val')]
-            dates = [valCol for valCol in columns if valCol.endswith('__date')]
+    vals = [valCol for valCol in columns if valCol.endswith('__val')]
+    dates = [valCol for valCol in columns if valCol.endswith('__date')]
 
-            if pos.endswith('__category') and pos != col:
+    for val in vals:
 
-                for val in vals:
+        for date in dates:
 
-                    for date in dates:
-
-                        for timeframe in timeframes:
-                            print(
-                                f'select {col}, {pos}, {val} from table group by {col}, {pos} order by {date} limit {timeframe}')
+            for timeframe in timeframes:
+                print(
+                    f'select {", ".join(col)}, {val} from table group by {", ".join(col)} order by {date} limit {timeframe}')
