923003ca240d0ed52d9c0e2ff278d4c7ceeb79a5
jkbngl <jakob.engl@hotmail.de>
2021-04-05 22:43:27 +0200

    support for multiple valcols

--- trend_detection/main.py
+++ trend_detection/main.py
@@ -9,35 +9,32 @@ import utils
 config = {
     'table': 'ffd.act_data',
     'columns_level': [{'level': 1, 'key': 'level1'}, {'level': 2, 'key': 'level2'}, {'level': 3, 'key': 'level3'}],
-    'valCol': 'amount',
-    'aggregationType': 'sum',
+    'valCols': [{'col': 'bobo', 'agg': 'sum'}, {'col': 'amount', 'agg': 'sum'}],
     'dateCol': "data_date",
-    'condition': "where level1 = 'EATING OUT'",
-    'timeframes': [5, 14, 21, 30, 60, 90, 180],
+    'condition': "where year > 2018",
+    'timeframes': [5, 30, 90],
     'db_type': 'postgres',
-    'columns': ['level1', 'level2', 'level3'],
+    'columns': ['level1', 'level2'],
 }
 
-where_conditions = query_builder.where_builder(config.get('columns'), config.get(
-    'table'), config.get('condition'), config.get('db_type'))
+
+cols = config.get('columns')
+table = config.get('table')
+condition = config.get('condition')
+dateCol = config.get('dateCol')
+valCols = config.get('valCols')
+timeframes = config.get('timeframes')
+db_type = config.get('db_type')
 
 
-print(where_conditions)
-
-categoryCombinations = iterator.combinations(
-    [col for col in config.get('columns')])
-
-
-queries = query_builder.query_builder(
-    categoryCombinations, config.get('timeframes'), config.get('valCol'), config.get('aggregationType'), config.get('dateCol'), config.get('table'), config.get('condition'))
+queries = query_builder.where_builder(
+    cols, table, condition, dateCol, valCols, timeframes, db_type)
 
 
 print('#')
 print(queries)
-print(categoryCombinations)
 
 for query in queries:
-    print(query)
 
     cursor = connector.getConnection(config.get('db_type'))
 
@@ -47,12 +44,27 @@ for query in queries:
 
     cursor.close()
 
-    vals = [float(res.get(config.get('valCol'))) for res in result]
+    # Check this for change in valcol
+    for i, col in enumerate(valCols):
+        key = result[0].get(col.get('col'))
+
+        if key:
+            vals = [float(key) for res in result]
+            break
+
     s = pd.Series(vals)
-    #s = s.rolling(10, min_periods=1).mean()
+    s = s.rolling(10, min_periods=1).mean()
 
-    trend, h, p, z, Tau, s, var_s, slope, intercept = trend_detector.trend_detector(
-        s)
+    if len(s) > 1:
+        try:
+            trend, h, p, z, Tau, s, var_s, slope, intercept = trend_detector.trend_detector(
+                s)
 
-    print(s)
-    print(trend)
+            if h:
+                print(query)
+                print(trend)
+        except Exception as e:
+            print(f"ERROR {e} with {len(s)} records for query {query}")
+    else:
+        # Cant calculate any trend with 1 record
+        pass
--- trend_detection/query_builder.py
+++ trend_detection/query_builder.py
@@ -3,18 +3,6 @@ import utils
 import pandas as pd
 
 
-def query_builder(categories, timeframes, valCol, aggregationType, dateCol, table, condition):
-
-    queries = []
-
-    for col in categories:
-
-        for timeframe in timeframes:
-            queries.append(
-                f'select {", ".join(col)}, {dateCol}, {aggregationType}({valCol}) as {valCol}  from {table} {condition} group by {", ".join(col)}, {dateCol} order by {dateCol} limit {timeframe}')
-    return queries
-
-
 def get_wheres(cols, table, where_condition, db_type):
     cursor = connector.getConnection(db_type)
 
@@ -31,18 +19,16 @@ def get_wheres(cols, table, where_condition, db_type):
     return result
 
 
-def where_builder(cols, table, where_condition, db_type):
+def where_builder(cols, table, condition, dateCol, valCols, timeframes, db_type):
 
-    combinations_ = get_wheres(cols, table, where_condition, db_type)
+    combinations = get_wheres(cols, table, condition, db_type)
 
-    combinations = [
-        {'site': 'BRU', 'orgarea': 'PMS', 'machinegroup': 'APH150'},
-        {'site': 'BRU', 'orgarea': 'FTT', 'machinegroup': 'APH150'},
-    ]
+    print(combinations)
 
     keys = []
     queries = []
 
+    # All possible columns, start with 1 and go up to the last, always adding columns
     for i in range(0, len(combinations[0].keys())):
         res = list(combinations[0].keys())[i]
 
@@ -51,20 +37,26 @@ def where_builder(cols, table, where_condition, db_type):
         df = pd.DataFrame(combinations)
 
         vals = df.drop_duplicates(keys)
-        print('+++++++++')
-        print(vals[keys])
 
+        print(f'UNIQUE on level ({len(keys)})')
+        print(len(vals))
+
+        # The unique vals per key have been calculated, create the queries per combinatio
         for index, row in vals.iterrows():
-            query = 'where 1 = 1'
 
-            print('~~~~~~~~~')
-            print(row)
+            print(index)
 
-            for j, key in enumerate(keys):
-                query += f" and {key} = '{row.get(key)}'"
+            for timeframe in timeframes:
+
+                for vcol in valCols:
+                    query = f'select {", ".join(keys)}, {dateCol}, {vcol.get("agg")}({vcol.get("col")}) as {vcol.get("col")} from {table} {condition} '
+
+                # Add multiple and conditions for multiple keys
+                for j, key in enumerate(keys):
+                    query += f" and {key} = '{row.get(key)}'"
+
+                query += f' group by {", ".join(keys)}, {dateCol} order by {dateCol} limit {timeframe}'
 
             queries.append(query)
-
-    print(queries)
 
     return queries
